f1c601a2f9c2cbbcb36a438dc9d9eec9e81ede50
paulpladziewicz <ppladziewicz@gmail.com>
2025-04-19 05:06:24 -0400

    making button more robust

--- projects/ngx-ui/src/lib/button/button.component.ts
+++ projects/ngx-ui/src/lib/button/button.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, HostBinding } from '@angular/core';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'ngx-button',
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
   @Input() label: string = 'Button';
   @Input() disabled: boolean = false;
-  @Input({ required: true }) type: string = 'button';
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
   @Output() buttonClick = new EventEmitter<Event>();
 
   onClick(event: Event): void {
